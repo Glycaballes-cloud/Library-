64bc8d90350955dc965504a76be929ccc861b355
Glycaballes-cloud <glaicaheartcaballes@gmail.com>
2026-07-07 20:27:34 -0700

    Refactor Employee class to manage library instead
    Updated manageEmployee method to manageLibrary and modified its output.

--- Employee.java
+++ Employee.java
@@ -1,18 +1,16 @@
-
 public class Employee extends Person {
     private String employeeId;
 
-    public Employee(String name, int age, String employeeId, String department) {
-        super(name, age);
+    public Employee(String name, int age, String employeeId) {
+        super( name, age);
         this.employeeId = employeeId;
     }
 
     public String getEmployeeId() {
         return employeeId;
     }
-
-    public void manageEmployee() {
-        System.out.println("Managing employee: " + getName());
-    }
     
+    public void manageLibrary() {
+        System.out.println("Managing the library by employee: " + getName() + " (ID: " + employeeId + ")");
+    }
 }
